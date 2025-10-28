6d5545d618ae5188b478b734a38790f992867682
SM03594 <santiago.margarit@alu.inspt.utn.edu.ar>
2025-10-28 05:26:22 -0300

    footer implementado

--- src/main/java/com/cyberkiosco/cyberkiosco_springboot/controller/IndexController.java
+++ src/main/java/com/cyberkiosco/cyberkiosco_springboot/controller/IndexController.java
@@ -2,14 +2,17 @@
 package com.cyberkiosco.cyberkiosco_springboot.controller;
 
 
+import java.time.LocalDate;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class IndexController {
+    
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { 
+        model.addAttribute("currentYear", LocalDate.now().getYear());
         return "index";
     }
 }
