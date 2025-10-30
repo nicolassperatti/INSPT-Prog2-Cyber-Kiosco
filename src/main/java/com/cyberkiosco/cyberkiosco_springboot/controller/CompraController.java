4065f23606c422af88b02638fed15d4db4fd34ef
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-10-30 13:00:29 -0300

    probando compra, parte 1, guardando carrito y producto con stock cambiado funcionando

---
+++ src/main/java/com/cyberkiosco/cyberkiosco_springboot/controller/CompraController.java
@@ -0,0 +1,78 @@
+package com.cyberkiosco.cyberkiosco_springboot.controller;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cyberkiosco.cyberkiosco_springboot.entity.Carrito;
+import com.cyberkiosco.cyberkiosco_springboot.entity.Producto;
+import com.cyberkiosco.cyberkiosco_springboot.entity.auxiliar.CarritoItemDTO;
+import com.cyberkiosco.cyberkiosco_springboot.service.CarritoProductoService;
+import com.cyberkiosco.cyberkiosco_springboot.service.CarritoService;
+import com.cyberkiosco.cyberkiosco_springboot.service.ProductoService;
+import com.cyberkiosco.cyberkiosco_springboot.service.UsuarioService;
+
+
+@Controller
+@RequestMapping("/compras")
+public class CompraController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @Autowired
+    private CarritoService carritoService;
+
+    @Autowired
+    private CarritoProductoService carritoProductoService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @ResponseBody
+    @GetMapping("/test")
+    public String getMethodName() {
+
+        //manejando producto primero
+        long idproducto = 1l;
+        long idproducto2 = 2l;
+        int cantidad1 = 2;
+        int cantidad2 = 3;
+        Producto producto = productoService.encontrarPorId(idproducto);
+        Producto producto2 = productoService.encontrarPorId(idproducto2);
+        // producto.restarStock(cantidad1);
+        // producto2.restarStock(cantidad2);
+        // productoService.guardarProducto(producto);
+        // productoService.guardarProducto(producto2);
+        // restar el stock del producto funciona sin problemas
+
+        //probando carrito ahora
+        List<CarritoItemDTO> cItemDTOs = new ArrayList<>();
+        cItemDTOs.add(new CarritoItemDTO(idproducto, cantidad1, producto.getPrecio()));
+        cItemDTOs.add(new CarritoItemDTO(idproducto2, cantidad2, producto2.getPrecio()));
+        double precio_total = this.obtenerPrecioTotal(cItemDTOs);
+        Carrito carrito = new Carrito();
+        carrito.setFecha_compra(LocalDateTime.now());
+        carrito.setPrecio_total(precio_total);
+        carrito.setUsuario(usuarioService.encontrarPorId(1l));
+        this.carritoService.guardarCarrito(carrito);
+        return "hola mundo";
+    }
+    
+
+    //funcion auxiliar
+    private double obtenerPrecioTotal(List<CarritoItemDTO> carritoItemDTOs){
+        double acum = 0.0;
+        for (CarritoItemDTO carritoItemDTO : carritoItemDTOs) {
+            acum += carritoItemDTO.getCantidad() * carritoItemDTO.getPrecio();
+        }
+        return Math.round((acum * 100.0)) / 100.0;
+    }
+}
